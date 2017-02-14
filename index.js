cdf99222fe473612d7146c056aa2909a60653083
Fabio <fabiocbinbutter@gmail.com>
2017-02-13 22:25:47 -0500

    I like constants

--- index.js
+++ index.js
@@ -1,57 +1,60 @@
 #! /usr/bin/env node
 
-var cliArgs = require('minimist')(process.argv.slice(
+const cliArgs = require('minimist')(process.argv.slice(
 		process.argv[0]=="dot-cli"
 		?1 //e.g. dot-cli --bla
 		:2 //e.g. node index.js --bla
 	));
-var glob=require("glob");
-var fs=require("fs");
-var path=require("path");
-var dot=require("dot");
+const glob=require("glob");
+const fs=require("fs");
+const path=require("path");
+const dot=require("dot");
 
-var templateGlob=cliArgs.template || cliArgs.t;
-var jsonGlob=cliArgs.json || cliArgs.j;
-var outpattern=cliArgs.output || cliArgs.o;
-var breakOnErrors=cliArgs.e;
+const templateGlob=cliArgs.template || cliArgs.t;
+const jsonGlob=cliArgs.json || cliArgs.j;
+const outpattern=cliArgs.output || cliArgs.o;
+const breakOnErrors=cliArgs.e;
+
 
 if(!templateGlob){console.error("template argument is required"); process.exit(1);}
 if(!jsonGlob){console.error("json argument is required"); process.exit(1);}
 
-var globOptions=cliArgs;
+const globOptions=cliArgs;
 
-var encoding=cliArgs.encoding||'utf8';
+const encoding=cliArgs.encoding||'utf8';
 
-var tFiles=glob.sync(templateGlob,globOptions);
-var jFiles=glob.sync(jsonGlob,globOptions);
+const tFiles=glob.sync(templateGlob,globOptions);
+const jFiles=glob.sync(jsonGlob,globOptions);
 
 if(!tFiles.length){console.warn("Warning: No template files were matched.")}
 if(!jFiles.length){console.warn("Warning: No json files were matched.")}
 
 tFiles.forEach(function(t){
-		var tFile=fs.readFileSync(t,{encoding:encoding});
-		var tFrag=filename(t)
+		const tFile=fs.readFileSync(t,{encoding:encoding});
+		const tFrag=filename(t)
+		var template;
 		try{
-				var template=dot.template(tFile);
+				template=dot.template(tFile);
 			}catch(e){
 				console.error(e);
 				if(breakOnErrors){process.exit(1);}
 				return;
 			}
 		jFiles.forEach(function(j){
-				var jFile=fs.readFileSync(j,{encoding:encoding});
-				var jFrag=filename(j);
+				const jFile=fs.readFileSync(j,{encoding:encoding});
+				const jFrag=filename(j);
+				var output;
 				try{
-						var output=template(JSON.parse(jFile));
+						output=template(JSON.parse(jFile));
 					}catch(e){
 						console.error(e);
 						if(breakOnErrors){process.exit(1);}
 						return;
 					}
 				if(outpattern){
-						var outpath=outpattern.replace("%t",tFrag).replace("%j",jFrag)
+						const outpath=outpattern.replace("%t",tFrag).replace("%j",jFrag)
 						outpath.split('/').slice(0,-1).reduce(function(accum,x,i){
-								accum.push(x); var path=accum.join('/')
+								accum.push(x); const path=accum.join('/')
 								//e.g. for a/b/c.txt => path: "a","a/b"
 								//e.g. for /a/b/c.txt => path: "", "/a", "/a/b"
 								if(path && !fs.existsSync(path)){fs.mkdirSync(path)}
