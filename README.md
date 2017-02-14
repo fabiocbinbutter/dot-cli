917511bf339a73f8bfb291d6243df08763ccee3f
Fabio <fabiocbinbutter@gmail.com>
2017-02-14 00:39:03 -0500

    Added json-less function compilation mode

--- README.md
+++ README.md
@@ -4,10 +4,11 @@ A command-line wrapper for the doT templating engine.
 * Install `npm install dot-cli`
 * Example stdout usage: `dot-cli --template="template.dot" --json="data.json"`
 * Example batch usage: `dot-cli --template="template.dot" --json="data/*.json" --output="out/%t %j.html"`
+* Example function compilation: `dot-cli --template="dot-files/*.dot" --output="dot-functions/%t.js"`
 
 #Arguments
 * `--template=<glob>` or `-t <glob>` . A glob specifying your dot file(s) (Required)
-* `--json=<glob>` or `-j <glob>` . A glob specifying your json data file(s) (Required)
+* `--json=<glob>` or `-j <glob>` . A glob specifying your json data file(s). If not provided, dot-cli outputs modules exporting the template functions
 * `--output=<pattern>` or `-o <pattern>` . A pattern specifying where to write the output. The tokens `%t` and `%j` are replaced with the filename of the respective template and json files, which is useful when a glob mathes multiple files. If this argument is not provided, the output will be to stdout.
 * `--encoding=...`. The encoding of your files, as accepted by fs.readFileSync. Default 'utf8'
 * `-e`. Set this flag to turn template compilation, JSON parsing, or template application errors into fatal errors. By default, such errors write to stderr and the script continues.
--- index.js
+++ index.js
@@ -17,21 +17,20 @@ const breakOnErrors=cliArgs.e;
 
 
 if(!templateGlob){console.error("template argument is required"); process.exit(1);}
-if(!jsonGlob){console.error("json argument is required"); process.exit(1);}
+const outputCompiledFunctions = !jsonGlob;
 
 const globOptions=cliArgs;
 
 const encoding=cliArgs.encoding||'utf8';
 
 const tFiles=glob.sync(templateGlob,globOptions);
-const jFiles=glob.sync(jsonGlob,globOptions);
+const jFiles=jsonGlob && glob.sync(jsonGlob,globOptions);
 
 if(!tFiles.length){console.warn("Warning: No template files were matched.")}
-if(!jFiles.length){console.warn("Warning: No json files were matched.")}
+if(jsonGlob && !jFiles.length){console.warn("Warning: No json files were matched.")}
 
 tFiles.forEach(function(t){
 		const tFile=fs.readFileSync(t,{encoding:encoding});
-		const tFrag=filename(t)
 		var template;
 		try{
 				template=dot.template(tFile);
@@ -40,33 +39,76 @@ tFiles.forEach(function(t){
 				if(breakOnErrors){process.exit(1);}
 				return;
 			}
-		jFiles.forEach(function(j){
-				const jFile=fs.readFileSync(j,{encoding:encoding});
-				const jFrag=filename(j);
-				var output;
-				try{
-						output=template(JSON.parse(jFile));
-					}catch(e){
-						console.error(e);
-						if(breakOnErrors){process.exit(1);}
-						return;
-					}
-				if(outpattern){
-						const outpath=outpattern.replace("%t",tFrag).replace("%j",jFrag)
-						outpath.split('/').slice(0,-1).reduce(function(accum,x,i){
-								accum.push(x); const path=accum.join('/')
-								//e.g. for a/b/c.txt => path: "a","a/b"
-								//e.g. for /a/b/c.txt => path: "", "/a", "/a/b"
-								if(path && !fs.existsSync(path)){fs.mkdirSync(path)}
-								return accum;
-							},[])
-						fs.writeFileSync(outpath,output);
-					}else{
-						process.stdout.write(output);
-					}
-			});
+		if(outputCompiledFunctions){
+				writeout(outpattern, t, "", "module.exports="+template.toString())
+			}else{
+				jFiles.forEach(function(j){
+						const jFile=fs.readFileSync(j,{encoding:encoding});
+						var output;
+						try{
+								output=template(JSON.parse(jFile));
+							}catch(e){
+								console.error(e);
+								if(breakOnErrors){process.exit(1);}
+								return;
+							}
+						writeout(outpattern, t, j, output)
+					});
+			}
 	});
+	if(outputCompiledFunctions){
+			writeout(outpattern, "index.", "", "module.exports={\n"
+					+tFiles.map(t=>
+							'"'+escapeStringLiteral(filename(t))+'":require("./'+escapeStringLiteral(filename(t))+'.js")'
+						).join("\n")+"\n}"
+				)
+		}
+
+function writeout(outpattern, t, j, output){
+		if(outpattern){
+				writeFileSyncPlusPath(
+						outpath(outpattern,t,j),
+						output
+					)
+			}else{
+				process.stdout.write(output);
+			}
+	}
+
+function writeFileSyncPlusPath(outpath, output){
+		outpath.split('/').slice(0,-1).reduce(function(accum,x,i){
+				accum.push(x); const path=accum.join('/')
+				//e.g. for a/b/c.txt => path: "a","a/b"
+				//e.g. for /a/b/c.txt => path: "", "/a", "/a/b"
+				if(path && !fs.existsSync(path)){fs.mkdirSync(path)}
+				return accum;
+			},[])
+		fs.writeFileSync(outpath,output);
+	}
+
+function outpath(pattern,t,j){
+		return outpattern.replace("%t",filename(t)).replace("%j",filename(j))
+	}
 
 function filename(x){
 		return x.split("/").slice(-1).join("").split(".").slice(0,-1).join(".");
+	}
+
+function escapeStringLiteral (string) {
+		return ('' + string).replace(/["'\\\n\r\u2028\u2029]/g, function (c) {
+				switch (c) {
+						case '"':
+						case "'":
+						case '\\':
+							return '\\' + c
+						case '\n':
+							return '\\n'
+						case '\r':
+							return '\\r'
+						case '\u2028':
+							return '\\u2028'
+						case '\u2029':
+							return '\\u2029'
+					}
+			});
 	}
